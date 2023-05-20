229c3cb00ae30b3c82b0cf6f625b606baceba136
ADDICTOR <2894515992@qq.com>
2023-05-20 08:33:50 +0800

    [feature : 实验plt.scatter绘图新功能]

--- demo/random_walk.py
+++ demo/random_walk.py
@@ -38,7 +38,24 @@ class RandomWalk():
             self.y_values.append(next_y)
 
 if __name__ == "__main__":
-    rw = RandomWalk()
-    rw.fill_walk()
-    plt.scatter(rw.x_values, rw. y_values, s=15)
-    plt.show()
+    while True:
+        rw = RandomWalk(5000)
+        rw.fill_walk()
+
+        plt.figure(dpi=128, figsize=(10, 6))
+
+        point_numbers = list(range(rw.num_points))
+        plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
+            cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+        # 突出起点与终点
+        plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+        plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',s=100)
+        plt.axes().get_xaxis().set_visible(False)
+        plt.axes().get_yaxis().set_visible(False)
+
+        plt.show()
+
+        keep_running = input("Make another walk?(y/n): ")
+        if keep_running == "n":
+            break
